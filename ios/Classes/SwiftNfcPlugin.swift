571aae7687baff066c9c93517938231323cc2ebc
Jesse de Wit <witdejesse@hotmail.com>
2019-02-04 13:13:18 +0100

    Created dummy implementation for iOS

--- ios/Classes/SwiftNfcPlugin.swift
+++ ios/Classes/SwiftNfcPlugin.swift
@@ -2,13 +2,49 @@ import Flutter
 import UIKit
 
 public class SwiftNfcPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "nfc", binaryMessenger: registrar.messenger())
-    let instance = SwiftNfcPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
+    let nfcAvailable = false
+    let nfcEnabled = false
 
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
-  }
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(
+            name: "plugins.passless.com/nfc",
+            binaryMessenger: registrar.messenger(),
+            codec: FlutterJSONMethodCodec.sharedInstance()
+        )
+        let instance = SwiftNfcPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+    }
+
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "configure":
+            onConfigure()
+            let props = [
+                "nfcAvailable": nfcAvailable,
+                "nfcEnabled": nfcEnabled
+            ]
+            result(props)
+        case "gotoNfcSettings":
+            result(
+                FlutterError(
+                    code: "UNAVAILABLE",
+                    message: "NFC settings unavailable",
+                    details: nil
+                )
+            )
+        default:
+            result(
+                FlutterError(
+                    code: "UNAVAILABLE",
+                    message: "Method not implemented.",
+                    details: nil
+                )
+            )
+        }
+    }
+    
+    private func onConfigure() {
+        
+    }
+
 }
